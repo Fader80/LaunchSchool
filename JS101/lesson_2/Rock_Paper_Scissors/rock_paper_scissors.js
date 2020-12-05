196ff76844e04f2c55b75737d909221fa46c2f79
Fuad Ahammed <56800859+Fader80@users.noreply.github.com>
2020-12-05 20:06:21 +0000

    completed version before changing while loop

---
+++ JS101/lesson_2/Rock_Paper_Scissors/rock_paper_scissors.js
@@ -0,0 +1,51 @@
+const readline = require('readline-sync');
+const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+
+function displayOutcome(userChoice, programChoice) {
+  prompt(`You chose ${userChoice}, computer chose ${programChoice}`);
+
+  if ((userChoice === 'rock' && programChoice === 'scissors') ||
+      (userChoice === 'paper' && programChoice === 'rock') ||
+      (userChoice === 'scissors' && programChoice === 'paper')) {
+    prompt('You win!');
+  } else if ((userChoice === 'rock' && programChoice === 'paper') ||
+             (userChoice === 'paper' && programChoice === 'scissors') ||
+             (userChoice === 'scissors' && programChoice === 'rock')) {
+    prompt('Computer wins!');
+  } else {
+    prompt("it's a tie!");
+  }
+}
+
+
+while (true) {
+  prompt(`choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice");
+    choice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+
+  displayOutcome(choice, computerChoice);
+
+  prompt('Do you want to play again (y/n)?');
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('please enter "yes" or "no".');
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0] !== 'y') break;
+
+}
