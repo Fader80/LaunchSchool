3e5691a2a5e730f51bccc6f113470c8185d75357
Fuad Ahammed <56800859+Fader80@users.noreply.github.com>
2020-12-10 22:03:30 +0000

    shortened input working

--- JS101/lesson_2/Rock_Paper_Scissors/rock_paper_scissors_bonus.js
+++ JS101/lesson_2/Rock_Paper_Scissors/rock_paper_scissors_bonus.js
@@ -12,21 +12,23 @@ function displayOutcome(userChoice, programChoice) {
 
   if (userChoice[0] === programChoice[0]) {
     prompt("it's a tie!");
-  } else if ((userChoice[0] === 'rock' &&
-  (programChoice[0] === 'scissors' || programChoice[0] === 'lizard')) ||
-  (userChoice[0] === 'paper' &&
-  (programChoice[0] === 'rock' || programChoice[0] === 'lizard')) ||
-  (userChoice[0] === 'scissors' &&
-  (programChoice[0] === 'paper' || programChoice[0] === 'lizard' )) ||
-  (userChoice[0] === 'lizard' &&
-  (programChoice[0] === 'paper' || programChoice[0] === 'spock')) ||
-  (userChoice[0] === 'spock' &&
-  (programChoice[0] === 'scissors' || programChoice[0] === 'rock'))) {
+  } else if ((userChoice[0] === 'r' &&
+  (programChoice[0] === 's' || programChoice[0] === 'l')) ||
+  (userChoice[0] === 'p' &&
+  (programChoice[0] === 'r' || programChoice[0] === 'l')) ||
+  (userChoice[0] === 's' &&
+  (programChoice[0] === 'p' || programChoice[0] === 'l' )) ||
+  (userChoice[0] === 'l' &&
+  (programChoice[0] === 'p' || programChoice[0] === 'S')) ||
+  (userChoice[0] === 'S' &&
+  (programChoice[0] === 's' || programChoice[0] === 'r'))) {
     prompt('You win!');
   } else {
     prompt('computer wins!');
   }
 }
+
+prompt('***Welcome to rock paper scissors lizard Spock!***\n');
 
 
 //the below loop is the version from 4. of 'Things to consider' - my solution
@@ -34,13 +36,32 @@ function displayOutcome(userChoice, programChoice) {
 let loopVariable = 'something';
 
 while (loopVariable) {
-  prompt(`choose one - if you want, you can type just the first letter: ${VALID_CHOICES.join(', ')}`);
+  prompt(`choose one - you can type just the first letter if you want:\n${VALID_CHOICES.join(', ')}`);
   let choice = readline.question();
 
-  while (!VALID_CHOICES.includes(choice)) {
+
+  let letterChoices = [];
+
+  for (let counter = 0; counter < VALID_CHOICES.length; counter++) {
+    letterChoices.push(VALID_CHOICES[counter][0]);
+  }
+
+
+  while (!(VALID_CHOICES.includes(choice) || letterChoices.includes(choice))) {
     prompt("That's not a valid choice");
     choice = readline.question();
   }
+
+
+  if (choice.length === 1) {
+    for (let counter2 = 0; counter2 < VALID_CHOICES.length; counter2++) {
+      if (choice === VALID_CHOICES[counter2][0]) {
+        choice = VALID_CHOICES[counter2];
+        break;
+      }
+    }
+  }
+
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
@@ -61,7 +82,3 @@ while (loopVariable) {
 
 }
 
-// for separate winner/loser/tie functions, could they slot into a higher-order function?
-// make them return 0 if condition is not met 
-
-// the other option I saw was to have an object with entries as (arrays?) of winning combinations
