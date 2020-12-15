fce13d4198d4a5f7da54231a2b830ba2aa841a4d
Fuad Ahammed <56800859+Fader80@users.noreply.github.com>
2020-12-15 20:38:54 +0000

    final version. amended as per TA's suggestions

--- JS101/lesson_2/Rock_Paper_Scissors/rock_paper_scissors_bonus.js
+++ JS101/lesson_2/Rock_Paper_Scissors/rock_paper_scissors_bonus.js
@@ -2,6 +2,8 @@ const { clear } = require('console');
 
 const readline = require('readline-sync');
 
+const WINNING_SCORE = 5;
+
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'Spock'];
 
 const WINNING_COMBOS = {
@@ -12,6 +14,14 @@ const WINNING_COMBOS = {
   Spock: ['rock', 'scissors']
 };
 
+let letterChoices = [];
+
+for (let counter = 0; counter < VALID_CHOICES.length; counter++) {
+  letterChoices.push(VALID_CHOICES[counter][0].toLowerCase()
+  + VALID_CHOICES[counter][1].toLowerCase());
+}
+
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -20,15 +30,24 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function programSelection() {
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  return  VALID_CHOICES[randomIndex];
+}
+
 function playerWins(userChoice, programChoice) {
   return WINNING_COMBOS[userChoice].includes(programChoice);
 }
 
+function sameChoice(userChoice, programChoice) {
+  return userChoice === programChoice;
+}
+
 
 function calculateScore(userChoice, programChoice) {
-  if (userChoice === programChoice) {
-    return;
-  } else if (playerWins(userChoice, programChoice)) {
+  if (sameChoice(userChoice, programChoice)) return;
+
+  if (playerWins(userChoice, programChoice)) {
     playerScore += 1;
   } else {
     computerScore += 1;
@@ -39,7 +58,7 @@ function calculateScore(userChoice, programChoice) {
 function displayRoundOutcome(userChoice, programChoice) {
   prompt(`You chose ${userChoice}, computer chose ${programChoice}`);
 
-  if (userChoice === programChoice) {
+  if (sameChoice(userChoice, programChoice)) {
     prompt("round is a tie");
   } else if (playerWins(userChoice, programChoice)) {
     prompt('You win the round!');
@@ -54,16 +73,16 @@ function displayScore() {
 
 
 function displayGrandWinner() {
-  if (playerScore === 5) {
+  if (playerScore === WINNING_SCORE) {
     prompt('^*^*CONGRATULATIONS - you are the Grand Winner!^*^*\n');
-  } else if (computerScore === 5) {
+  } else if (computerScore === WINNING_SCORE) {
     prompt('COMMISERATIONS - computer is the Grand Winner :-(\n');
   }
 }
 
 
 function resetScoreGrandWin() {
-  if (playerScore === 5 || computerScore === 5) {
+  if (playerScore === WINNING_SCORE || computerScore === WINNING_SCORE) {
     playerScore = 0;
     computerScore = 0;
   }
@@ -75,32 +94,26 @@ prompt('***Welcome to rock paper scissors lizard Spock!***\n              > Firs
 
 //the below loop is the version from 4. of 'Things to consider' - my solution
 //to remove the break statement was to set this loopVariable to a falsy value
-let loopVariable = 'something';
+let playGame = 'something';
 
-while (loopVariable) {
+while (playGame) {
 
   displayScore();
 
-  prompt(`Choose one of the following - you may type just the first letter:\n${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
-
-
-  let letterChoices = [];
-
-  for (let counter = 0; counter < VALID_CHOICES.length; counter++) {
-    letterChoices.push(VALID_CHOICES[counter][0]);
-  }
+  prompt(`Choose one of the following - you may type just the first two letters:\n${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question().toLowerCase();
 
 
   while (!(VALID_CHOICES.includes(choice) || letterChoices.includes(choice))) {
     prompt("That's not a valid choice");
-    choice = readline.question();
+    choice = readline.question().toLowerCase();
   }
 
 
-  if (choice.length === 1) {
+  if (choice.length === 2) {
     for (let counter2 = 0; counter2 < VALID_CHOICES.length; counter2++) {
-      if (choice === VALID_CHOICES[counter2][0]) {
+      if (choice === VALID_CHOICES[counter2][0].toLowerCase()
+        + VALID_CHOICES[counter2][1].toLowerCase()) {
         choice = VALID_CHOICES[counter2];
         break;
       }
@@ -108,8 +121,7 @@ while (loopVariable) {
   }
 
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+  let computerChoice = programSelection();
 
   displayRoundOutcome(choice, computerChoice);
 
@@ -129,8 +141,11 @@ while (loopVariable) {
     answer = readline.question().toLowerCase();
   }
 
-  if (answer[0] !== 'y') loopVariable = '';
+  if (answer[0] !== 'y') playGame = '';
 
   clear();
 
 }
+
+
+//also complete ET's suggestion for case insensitivity
