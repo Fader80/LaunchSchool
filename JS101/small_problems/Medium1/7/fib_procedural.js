8a564df8bac9043024f7e26592501fcd628e29d0
Fuad Ahammed <56800859+Fader80@users.noreply.github.com>
2023-01-08 21:22:39 +0000

    amended fib solution

--- JS101/small_problems/Medium1/7/fib_procedural.js
+++ JS101/small_problems/Medium1/7/fib_procedural.js
@@ -1,11 +1,16 @@
-function fibonacci(n) {
+function fibonacci(argNum) {
+
+  if (argNum < 3) {
+    return argNum;
+  }
+
   let f1 = 1;
 
   let f2 = 1;
 
   let fib;
 
-  for (let idx = 2; idx < n; idx++) {
+  for (let idx = 2; idx < argNum; idx++) {
     fib = f1 + f2;
     f1 = f2;
     f2 = fib;
