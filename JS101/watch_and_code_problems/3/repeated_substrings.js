5fb310e53452328b23320cc4f360802cd6d94e93
Fuad Ahammed <fuadahammed@hotmail.com>
2024-12-02 17:58:53 +0000

    ls watch others code 3 repeated substrings

---
+++ JS101/watch_and_code_problems/3/repeated_substrings.js
@@ -0,0 +1,24 @@
+function repeatedSubstringPattern(argStr) {
+  let allSubStrings = [];
+
+  for (let idx = 0; idx < argStr.length; idx++) {
+    for (let jdx = idx + 1; jdx <= argStr.length; jdx++) {
+      let subStr = argStr.slice(idx, jdx);
+      allSubStrings.push(subStr);
+    }
+  }
+
+  allSubStrings = allSubStrings.filter(currSub => currSub !== argStr);
+
+
+  for (let index = 0; index < allSubStrings.length; index++) {
+    let currSubStr = allSubStrings[index];
+    let targStr = '';
+    while (targStr.length < argStr.length) {
+      targStr += currSubStr;
+    }
+
+    if (targStr === argStr) return true;
+  }
+  return false;
+}
